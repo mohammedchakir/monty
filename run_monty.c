1282dc5f63b1157bedf4d1a323dbb20bb6df1966
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-14 20:33:15 +0100

    Update run_monty.c

--- run_monty.c
+++ run_monty.c
@@ -116,7 +116,7 @@ int run_monty(FILE *script_fd)
 	if (init_stack(&stack) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 
-	while (*get_int(&line, &len, script_fd) != -1)
+	while (get_int(&line, &len, script_fd) != -1)
 	{
 		line_number++;
 		op_toks = strtow(line, DELIMS);
