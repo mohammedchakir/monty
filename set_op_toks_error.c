db166168bddcebc189a6df2fa5b49ad8c2a194de
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-14 08:55:09 +0100

    Update set_op_toks_error.c

--- set_op_toks_error.c
+++ set_op_toks_error.c
@@ -6,21 +6,21 @@
  */
 void set_op_tok_error(int error_code)
 {
-	int toks_len = 0, i = 0;
+	int tok_len = 0, a = 0;
 	char *exit_str = NULL;
 	char **new_toks = NULL;
 
-	toks_len = token_arr_len();
-	new_toks = malloc(sizeof(char *) * (toks_len + 2));
+	tok_len = token_arr_len();
+	new_toks = malloc(sizeof(char *) * (tok_len + 2));
 	if (!op_toks)
 	{
 		malloc_error();
 		return;
 	}
-	while (i < toks_len)
+	while (a < tok_len)
 	{
-		new_toks[i] = op_toks[i];
-		i++;
+		new_toks[a] = op_toks[a];
+		a++;
 	}
 	exit_str = get_int(error_code);
 	if (!exit_str)
@@ -29,8 +29,8 @@ void set_op_tok_error(int error_code)
 		malloc_error();
 		return;
 	}
-	new_toks[i++] = exit_str;
-	new_toks[i] = NULL;
+	new_toks[a++] = exit_str;
+	new_toks[a] = NULL;
 	free(op_toks);
 	op_toks = new_toks;
 }
