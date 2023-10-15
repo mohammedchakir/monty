2e0c9f26bc648c1fffedab5b199e5c035de76c00
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-15 09:41:18 +0100

    Update stack_op.c

--- stack_op.c
+++ stack_op.c
@@ -1,9 +1,10 @@
 #include "monty.h"
 
+
 /**
- * mul_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * mul_nodes - Multiplies the values of the top two elements in the stack.
+ * @stack: The pointer to a pointer pointing to the top node of the stack.
+ * @line_number: An integer representing the line number of the opcode.
  */
 void mul_nodes(stack_t **stack, unsigned int line_number)
 {
@@ -21,9 +22,9 @@ void mul_nodes(stack_t **stack, unsigned int line_number)
 
 
 /**
- * mod_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * mod_nodes - Computes the modulo of the top two elements in the stack.
+ * @stack: The pointer to a pointer pointing to the top node of the stack.
+ * @line_number: An integer showing the line number of the opcode.
  */
 void mod_nodes(stack_t **stack, unsigned int line_number)
 {
