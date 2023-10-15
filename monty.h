ea0388646ca16f0a5fcd3b7d6b4ee447c94d218b
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-15 09:23:37 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -42,43 +42,39 @@ typedef struct instruction_s
 extern stack_t *head;
 typedef void (*op_func)(stack_t **, unsigned int);
 
-/*file operations*/
-void open_file(char *file_name);
-int parse_line(char *buffer, int line_number, int format);
-void read_file(FILE *);
-int len_chars(FILE *);
-void find_func(char *, char *, int, int);
 
-/*Stack operations*/
+/*All functions employed in Monty-project*/
+int len_chars(FILE *);
 stack_t *create_node(int n);
+int parse_line(char *buffer, int line_number, int format);
+
+void find_func(char *, char *, int, int);
+void read_file(FILE *);
+void open_file(char *file_name);
+void call_fun(op_func, char *, char *, int, int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+void print_char(stack_t **, unsigned int);
 void free_nodes(void);
 void print_stack(stack_t **, unsigned int);
 void add_to_stack(stack_t **, unsigned int);
 void add_to_queue(stack_t **, unsigned int);
-
-void call_fun(op_func, char *, char *, int, int);
-
-void print_top(stack_t **, unsigned int);
-void pop_top(stack_t **, unsigned int);
-void nop(stack_t **, unsigned int);
-void swap_nodes(stack_t **, unsigned int);
-
-/*Math operations with nodes*/
-void add_nodes(stack_t **, unsigned int);
-void sub_nodes(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+void err(int error_code, ...);
+void more_err(int error_code, ...);
 void div_nodes(stack_t **, unsigned int);
 void mul_nodes(stack_t **, unsigned int);
 void mod_nodes(stack_t **, unsigned int);
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
 
-/*String operations*/
-void print_char(stack_t **, unsigned int);
-void print_str(stack_t **, unsigned int);
-void rotl(stack_t **, unsigned int);
 
-/*Error hanlding*/
-void err(int error_code, ...);
-void more_err(int error_code, ...);
-void string_err(int error_code, ...);
-void rotr(stack_t **, unsigned int);
 
-#endif
+
+
+#endif /* __MONTY_H__ */
