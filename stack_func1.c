01dd0eedbdc0e72339e4a8f79e92f11d42593bc7
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-15 09:33:01 +0100

    Update stack_func1.c

--- stack_func1.c
+++ stack_func1.c
@@ -2,9 +2,9 @@
 
 
 /**
- * add_to_stack - Adds a node to the stack.
- * @new_node: Pointer to the new node.
- * @ln: Interger representing the line number of of the opcode.
+ * add_to_stack - Pushes a node into the stack.
+ * @new_node: The pointer to the new node.
+ * @ln: An integer showing the line number of the opcode.
  */
 void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
@@ -25,9 +25,9 @@ void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 
 
 /**
- * print_stack - Adds a node to the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: line number of  the opcode.
+ * print_stack - Displays the contents of a stack.
+ * @stack: The pointer to a pointer pointing to the top node of the stack.
+ * @lineNumber: The line number of the opcode.
  */
 void print_stack(stack_t **stack, unsigned int line_number)
 {
@@ -49,6 +49,12 @@ void print_stack(stack_t **stack, unsigned int line_number)
  * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
+/**
+ * pop_top - Removes and returns the top node from the stack.
+ * @stack: The pointer to a pointer pointing to the top node of the stack.
+ * @lineNumber: An integer showing the line number of the opcode.
+ */
+
 void pop_top(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
@@ -63,10 +69,11 @@ void pop_top(stack_t **stack, unsigned int line_number)
 	free(tmp);
 }
 
+
 /**
- * print_top - Prints the top node of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * print_top - Displays the value of the top node of the stack.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @lineNumber: An integer showing the line number of the opcode.
  */
 void print_top(stack_t **stack, unsigned int line_number)
 {
