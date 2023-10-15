ed55d4a3de65f4a85c0a88dffa4da36633b815c1
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-15 10:48:06 +0100

    Update and rename stack_strings.c to strings_stack_func.c

--- strings_stack_func.c
+++ strings_stack_func.c
@@ -8,15 +8,15 @@
  */
 void print_char(stack_t **stack, unsigned int line_number)
 {
-	int ascii;
+	int val;
 
 	if (stack == NULL || *stack == NULL)
 		string_err(11, line_number);
 
-	ascii = (*stack)->n;
-	if (ascii < 0 || ascii > 127)
+	val = (*stack)->n;
+	if (val < 0 || val > 127)
 		string_err(10, line_number);
-	printf("%c\n", ascii);
+	printf("%c\n", val);
 }
 
 /**
@@ -26,8 +26,8 @@ void print_char(stack_t **stack, unsigned int line_number)
  */
 void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
-	int ascii;
-	stack_t *tmp;
+	int val;
+	stack_t *temp;
 
 	if (stack == NULL || *stack == NULL)
 	{
@@ -35,14 +35,14 @@ void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
 		return;
 	}
 
-	tmp = *stack;
-	while (tmp != NULL)
+	temp = *stack;
+	while (temp != NULL)
 	{
-		ascii = tmp->n;
-		if (ascii <= 0 || ascii > 127)
+		val = temp->n;
+		if (val <= 0 || val > 127)
 			break;
-		printf("%c", ascii);
-		tmp = tmp->next;
+		printf("%c", val);
+		temp = temp->next;
 	}
 	printf("\n");
 }
@@ -55,17 +55,17 @@ void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
  */
 void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
-	stack_t *tmp;
+	stack_t *temp;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		return;
 
-	tmp = *stack;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-	tmp->next = *stack;
-	(*stack)->prev = tmp;
+	temp->next = *stack;
+	(*stack)->prev = temp;
 	*stack = (*stack)->next;
 	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
@@ -79,19 +79,19 @@ void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
  */
 void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
-	stack_t *tmp;
+	stack_t *temp;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		return;
 
-	tmp = *stack;
+	temp = *stack;
 
-	while (tmp->next != NULL)
-		tmp = tmp->next;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-	tmp->next = *stack;
-	tmp->prev->next = NULL;
-	tmp->prev = NULL;
-	(*stack)->prev = tmp;
-	(*stack) = tmp;
+	temp->next = *stack;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	(*stack)->prev = temp;
+	(*stack) = temp;
 }
